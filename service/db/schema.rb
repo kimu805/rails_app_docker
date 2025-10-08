e8810f990914322a6a34c84b14432725ff1f5b57
kimu805 <kimukimuok805@gmail.com>
2025-10-08 16:28:48 +0900

    commit

--- service/db/schema.rb
+++ service/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_08_061222) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_08_065725) do
   create_table "hellos", charset: "utf8mb3", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_08_061222) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
