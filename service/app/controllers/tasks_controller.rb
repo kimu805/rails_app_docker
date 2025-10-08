4972ff168d9549c0a91b2eca1f4875e68cff55e1
kimu805 <kimukimuok805@gmail.com>
2025-10-08 14:38:23 +0900

    commit

--- service/app/controllers/tasks_controller.rb
+++ service/app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [ :show, :edit, :update, :destroy ]
+
   def index
     @tasks = Task.all
   end
@@ -16,11 +18,25 @@ class TasksController < ApplicationController
   def edit
   end
 
+  def update
+    @task.update!(task_params)
+    redirect_to task_path(@task), notice: "タスク「#{@task.name}」を編集しました。"
+  end
+
   def show
+  end
+
+  def destroy
+    @task.destroy!
+    redirect_to root_path, notice: "タスク「#{@task.name}」を削除しました。"
   end
 
   private
   def task_params
     params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = Task.find_by(id: params[:id])
   end
 end
