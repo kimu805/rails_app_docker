c2da867cb236c315694c2c4463f723b301367297
kimu805 <kimukimuok805@gmail.com>
2025-10-08 13:35:08 +0900

    commit

--- service/app/controllers/tasks_controller.rb
+++ service/app/controllers/tasks_controller.rb
@@ -7,9 +7,20 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to root_path, notice: "タスク「#{task.name}」を登録しました。"
+  end
+
   def edit
   end
 
   def show
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
