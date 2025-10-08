da4be5d0a0e79c34c680d825cfe74550888a1669
kimu805 <kimukimuok805@gmail.com>
2025-10-08 11:41:14 +0900

    commit

--- service/config/routes.rb
+++ service/config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get "tasks/index"
-  get "tasks/new"
-  get "tasks/edit"
-  get "tasks/show"
-  root to: "messages#index"
-  resources :messages
+  root to: "tasks#index"
+  resources :tasks
 end
