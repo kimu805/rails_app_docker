7e661447bacc3483634db5196d0abbd6a74ed5de
kimu805 <kimukimuok805@gmail.com>
2025-10-11 16:36:46 +0900

    commit

--- service/app/controllers/application_controller.rb
+++ service/app/controllers/application_controller.rb
@@ -2,9 +2,14 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   helper_method :current_user
+  before_action :login_required
 
   private
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def login_required
+    redirect_to login_path unless current_user
   end
 end
