c7f7f835a77c4174ca26fbb21423298f246926c2
kimu805 <kimukimuok805@gmail.com>
2025-10-14 10:45:45 +0900

    commit

--- service/app/controllers/admin/users_controller.rb
+++ service/app/controllers/admin/users_controller.rb
@@ -1,5 +1,7 @@
 class Admin::UsersController < ApplicationController
   before_action :set_user, only: [ :show, :edit, :update, :destroy ]
+  before_action :require_admin
+
   def index
     @users = User.all
   end
@@ -44,5 +46,9 @@ class Admin::UsersController < ApplicationController
 
   def set_user
     @user = User.find_by(id: params[:id])
+  end
+
+  def require_admin
+    redirect_to root_path unless current_user.admin?
   end
 end
